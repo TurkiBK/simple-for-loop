873e1fdc16f1e763a36d75479867a2509cea3c58
TurkiBK <56528434+TurkiBK@users.noreply.github.com>
2019-10-15 18:01:34 -0700

    Update PointMain.java

--- PointMain.java
+++ PointMain.java
@@ -1,9 +1,10 @@
 
-public class PointMain {
+public class test {
 
 	public static void main(String[] args) {
 	
 		for(int x=0 , x>=10; X++){
+			for(int x=0 , x>=10; X++){
         }
 
 	System.out.println(x);

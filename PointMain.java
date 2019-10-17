78b5c1d9458ce88e2b2f7d364951e63b31e18e69
TurkiBK <56528434+TurkiBK@users.noreply.github.com>
2019-10-17 08:52:16 -0700

    fix bug and ready to go

--- PointMain.java
+++ PointMain.java
@@ -6,8 +6,8 @@ public class test {
 
 	public static void main(String[] args) {
 	
-		for(int x=0 , x>=10, x++){
-			for(int x=0 , x>=10, x++){
+		for(int x=0;  x>=10; x++){
+			for(int x=0;  x>=10; x++){
         }
 
 	System.out.println(x*x);
